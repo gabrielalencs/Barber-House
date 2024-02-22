110903903a3aa7b9e50bbfb444d329631802f5b4
Gabriel de Alencar <gabrieldealencar.dev@gmail.com>
2024-02-22 20:39:22 -0300

    adding style patterns to the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,18 @@
 module.exports = {
   content: ["./index.html", "./src/js/script.js"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+        'dm-sans': ['DM Sans','sans-serif']
+      }, 
+      colors: {
+        'brown-primary': '#AE3824',
+        'brown-secondary': '#23120F',
+        'white-primary': '#FAF9F9',
+        'gray-primary': '#AFAFAF'
+      }
+    },
   },
-  plugins: [],
 }
 

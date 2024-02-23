b7e500984227c7e7277fc943112a63ddd24767bc
Gabriel de Alencar <gabrieldealencar.dev@gmail.com>
2024-02-22 23:17:35 -0300

    adjust the header responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: [ "./src/js/script.js", "./index.html"],
   theme: {
+    screens: {
+      'max-lg': {'max': '992px'},
+      'min-lg': {'min': '992px'},
+    },
     extend: {
       fontFamily: {
         'poppins': ['Poppins', 'sans-serif'],
@@ -16,8 +20,9 @@ module.exports = {
       spacing: {
         '1280': '1280px',
         '1000': '-1000px'
-      }
-    },
+      },
+
+    }
   },
 }
 

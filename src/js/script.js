1579c8fb841cb755007a1c1b67edaeb17cfbc662
Gabriel de Alencar <gabrieldealencar.dev@gmail.com>
2024-03-09 15:13:31 -0300

    finalizing mobile version of the comments section

--- src/js/script.js
+++ src/js/script.js
@@ -11,10 +11,24 @@ function toggleMenu() {
 }
 
 // Swiper JS
-let swiper = new Swiper(".mySwiper", {
-    spaceBetween: 30,
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 3,
+    grabCursor: true,
+    spaceBetween: 20,
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
     },
+    breakpoints: {
+        50: {
+            slidesPerView: 1,
+        },
+        481: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        668: {
+            slidesPerView: 3,
+        }
+    }
 });
